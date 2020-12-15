adfae2aedc97254d26022138199b96a3fe7876ee
Augustin Godinot <augustin.godinot@ens-paris-saclay.fr>
2020-12-15 11:27:46 +0100

    Correct threads declaration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-os.environ['LK_NUM_PROCS'] = '12,2'
+os.environ['LK_NUM_PROCS'] = '20,2'
 
 import luigi
 import numba
@@ -15,33 +15,32 @@ def main():
     msd_dataset = MsdDataset(n_users=0)
     n_factors_values = list(range(50, 300, 20)) + [500,]
     # n_factors_values = list(range(30, 140, 10))
-    n_factors_values = [30, 60, 100, 150, 200, 300, 400, 500, 1_000]
-    regularization_values = [.01, .02, .03, .04, .05, .1, .3, .5, .7, 1, 5]
+    n_factors_values = [200, 300, 400, 500, 800, 1_000]
+    regularization_values = [.001, .005, .01, .02, .03, .04, .05]
 
     tasks = [
         DatasetInfo(dataset=msd_dataset),
+        
         PlotUserVolumeHistogram(dataset=msd_dataset),
         PlotUsersDiversitiesHistogram(dataset=msd_dataset),
         PlotTagsDiversitiesHistogram(dataset=msd_dataset),
-        BuildDatasetGraph(dataset=msd_dataset),
-        ComputeUsersDiversities(dataset=msd_dataset),
+        
         TrainTestInfo(dataset=msd_dataset),
     ]
     tasks += [
-        PlotDiversityVsLatentFactors(dataset=msd_dataset, n_factors_values=n_factors_values),
-        PlotDiversityIncreaseVsLatentFactors(dataset=msd_dataset, n_factors_values=n_factors_values),
-        TuneModelHyperparameters(dataset=msd_dataset, model_n_factors_values=n_factors_values, model_regularization_values=regularization_values),
-        PlotDiversityVsRegularization(dataset=msd_dataset, model_n_factors=100, model_regularization_values=regularization_values),
-        PlotModelTuning(dataset=msd_dataset, model_n_factors_values=n_factors_values, model_regularization_values=regularization_values),
-        # CollectAllModelFigures(dataset=msd_dataset),
+    #     PlotDiversityVsLatentFactors(dataset=msd_dataset, n_factors_values=n_factors_values),
+    #     PlotDiversityIncreaseVsLatentFactors(dataset=msd_dataset, n_factors_values=n_factors_values),
+    #     TuneModelHyperparameters(dataset=msd_dataset, model_n_factors_values=n_factors_values, model_regularization_values=regularization_values),
+    #     PlotDiversityVsRegularization(dataset=msd_dataset, model_n_factors=100, model_regularization_values=regularization_values),
+        PlotModelTuning(dataset=msd_dataset, model_n_factors_values=n_factors_values, model_n_iterations=30, model_regularization_values=regularization_values),
     ]
-    tasks += [PlotRecommendationsUsersDiversitiesHistogram(dataset=msd_dataset, model_n_factors=n) for n in n_factors_values]
-    tasks += [PlotDiversitiesIncreaseHistogram(dataset=msd_dataset, model_n_factors=n) for n in n_factors_values]
+    # tasks += [PlotRecommendationsUsersDiversitiesHistogram(dataset=msd_dataset, model_n_factors=n) for n in n_factors_values]
+    # tasks += [PlotDiversitiesIncreaseHistogram(dataset=msd_dataset, model_n_factors=n) for n in n_factors_values]
     # tasks += [EvaluateModel(dataset=msd_dataset, model_n_factors=n, model_n_iterations=30) for n in n_factors_values]
     
     # tasks = [DeleteAllModelFigures(dataset=msd_dataset),]
 
-    luigi.build(tasks, local_scheduler=True, log_level='INFO')
+    luigi.build(tasks, local_scheduler=False, log_level='INFO')
 
 
 if __name__ == '__main__':
