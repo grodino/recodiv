501928224eae5207ed499a139826d5d7d9960576
Augustin Godinot <augustin.godinot@ens-paris-saclay.fr>
2021-03-06 12:26:07 +0100

    Refractor animation and create cli tool

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
-import os
+# import os
 # os.environ['LK_NUM_PROCS'] = '10,2'
 
-import luigi
-import numba
-from lenskit.util import log_to_stderr
+from socket import MsgFlag
+import click
 
+from automation.config import *
+from automation.interactive import *
 from automation.msd_dataset import *
 
-CONFIDENCE_FACTOR = 40
-N_ITERATIONS = 10
-N_RECOMMENDATIONS = 50
-N_FACTORS_VALUES = [5, 20, 50, 60, 70, 80, 200, 500, 1_000, 3_000]
-REGULARIZATION_VALUES = [.005, .01, 1.0, 10.0, 100.0, 200.0, 5_000.0, 1e5, 1e6]
-# TODO: change these values when using the better dataset (not the confidence pre-corrected ...)
-OPT_N_FACTORS = 500
-OPT_REGULARIZATION = 5_000.0
 
+@click.group()
+def cli():
+    pass
 
-def report_figures(msd_dataset):
+@cli.command()
+@click.option(
+    '--local-scheduler', 
+    default=False, 
+    help='Use a luigi local scheduler for the tasks execution'
+)
+def report_figures(local_scheduler):
+    msd_dataset = MsdDataset(n_users=10_000)
+
     tasks = []
 
     # General information about the dataset
@@ -142,7 +146,7 @@ def report_figures(msd_dataset):
             model_n_iterations=N_ITERATIONS,
             n_factors_values=[5, 20, 500, 3_000],
             model_regularization=OPT_REGULARIZATION,
-            n_recommendations_values=[10, 20, 30, 40, 50, 75, 100, 150, 200, 500, 1000]   
+            n_recommendations_values=N_RECOMMENDATIONS_VALUES
         )
     ]
 
@@ -169,100 +173,34 @@ def report_figures(msd_dataset):
     #     )
     # ]
 
+    luigi.build(tasks, local_scheduler=local_scheduler, log_level='INFO', scheduler_host='127.0.0.1')
 
-    return tasks
 
-def interactive(dataset):
-    n_iterations = N_ITERATIONS
-    regularization = OPT_REGULARIZATION
-    n_recommendations = N_RECOMMENDATIONS
+@cli.group()
+def interactive():
+    pass
 
-    merged = ComputeRecommendationDiversityVsUserDiversityVsLatentFactors(
-        dataset=dataset,
-        model_n_iterations=n_iterations,
-        model_n_factors_values=N_FACTORS_VALUES,
-        model_regularization=regularization,
-        n_recommendations=n_recommendations
-    ).run()
 
-    fig = px.scatter(
-        merged, 
-        x='diversity', 
-        y='reco_diversity',
-        hover_data=['user'],
-        custom_data=['n_factors'],
-        animation_frame='n_factors',
-        animation_group='user',
-        marginal_x='histogram',
-        marginal_y='histogram',
-        color='volume', 
-        color_continuous_scale=px.colors.sequential.Viridis,
-        width=float('inf'),
-        height=900,
-        title='Effect of user diversity on their recommendation diversity',
-        labels={
-            'diversity': 'User individual diversity',
-            'reco_diversity': 'Recommendation diversity',
-            'volume': 'log10(volume)',
-        }
-    )
-    fig.update_layout(coloraxis_colorbar=dict(
-        title='volume',
-        tickvals=[1, 1.477, 2, 2.477, 3],
-        ticktext=['10', '30', '100', '300', '1000'],
-    ))
-    
-    app = dash.Dash()
-    app.layout = html.Div([
-        dcc.Graph(
-            id='basic-interactions',
-            figure=fig
-        ),
-
-        html.Div(className='row', children=[
-            html.Div([
-                dcc.Markdown("""
-                    **Click Data**
-
-                    Click on points in the graph.
-                """),
-                html.Pre(id='click-data'),
-            ], className='three columns'),
-        ]),
-    ])
-
-    @app.callback(
-        Output('click-data', 'children'),
-        Input('basic-interactions', 'clickData'))
-    def display_click_data(click_data):
-        if not click_data:
-            return
-
-        point = click_data['points'][0]
-        user_id = point['id']
-        n_factors = int(point['customdata'][0])
-
-        user_info = AnalyseUser(
-            user_id=user_id,
-            dataset=dataset,
-            model_n_iterations=n_iterations,
-            model_n_factors=n_factors,
-            model_regularization=regularization,
-            n_recommendations=n_recommendations
-        ).run()
-
-        return json.dumps(user_info, indent=2)
-
-    app.run_server(debug=True, use_reloader=False) 
-
-def main():
+@interactive.command()
+@click.option(
+    '--local-scheduler', 
+    default=False, 
+    help='Use a luigi local scheduler for the tasks execution'
+)
+def latent_factors(local_scheduler):
     msd_dataset = MsdDataset(n_users=10_000)
+    reco_div_vs_user_div_vs_latent_factors(msd_dataset, local_scheduler)
 
-    tasks = []
-    tasks += report_figures(msd_dataset)
 
-    # luigi.build(tasks, local_scheduler=False, log_level='INFO', scheduler_host='127.0.0.1')
-    interactive(msd_dataset)
+@interactive.command()
+@click.option(
+    '--local-scheduler', 
+    default=False, 
+    help='Use a luigi local scheduler for the tasks execution'
+)
+def recommendation_volume(local_scheduler):
+    msd_dataset = MsdDataset(n_users=10_000)
+    reco_div_vs_user_div_vs_reco_volume(msd_dataset, local_scheduler)
 
 if __name__ == '__main__':
-    main()
+    cli()
